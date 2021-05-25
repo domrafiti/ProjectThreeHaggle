c72bd095a929c654af5fa4f046f708dfa8f91d98
bahuisken <brice.huisken@gmail.com>
2021-05-24 22:44:45 -0600

    trying to connect to db

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
 
-export function Main() {
+function Main() {
+  // Setting our component's initial state
+  const [users, setUsers] = useState([]);
+  const [formObject, setFormObject] = useState({});
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  // Loads all books and sets them to books
+  function loadUsers() {
+    API.getListings()
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  }
+
   return (
-    <div>
-      <p>Main</p>
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          {/* <img src="/img/handshake.jpeg" className="d-block w-100" alt="Landing page image"> */}
+          <div className="carousel-caption carousel-caption-custom">
+            <h5>Welcome to Haggle! </h5>
+
+            {users.map((user) => (
+              <div key={user._id}>
+                <strong>{user.name}</strong>
+              </div>
+            ))}
+
+            <p>Scroll through to see current listings!</p>
+          </div>
+        </div>
+
+        {/* {{#each listings as |listing| }}
+    <div className="carousel-item">
+      <img src="{{image_path}}" className="d-block w-100" alt="...">
+      <div className="carousel-caption carousel-caption-custom">
+        <h5>{{title}}</h5>
+        <p>{{description}}</p>
+      </div>
+    </div>
+    {{/each}} */}
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
   );
 }
--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Book: require("./user"),
+  User: require("./user"),
 };
