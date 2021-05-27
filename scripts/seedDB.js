04730da50c026b9cbea30d8e36d591c737766ac6
Dom Rafiti <dom.rafiti@gmail.com>
2021-05-26 20:42:27 -0600

    updating seed file with more records

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -16,8 +16,8 @@ const userSeed = [
     email: "dom.rafiti@gmail.com",
     password: "password12345",
     picture_path: "/tmp_uploads/f66a3bc8-0b42-49a2-9671-31045a707531-man 5.jpg",
-    total_ratings: ["3", "3", "3"],
-    average_rating: 3,
+    total_ratings: ["3", "4", "4"],
+    average_rating: 4,
     created_dt: Date.now(),
     favorites: [],
     listings: [
@@ -42,6 +42,89 @@ const userSeed = [
       },
     ],
   },
+  {
+    name: "Giovany",
+    email: "giochavez320@gmail.com",
+    password: "password12345",
+    picture_path: "/tmp_uploads/f66a3bc8-0b42-49a2-9671-31045a707531-man 5.jpg",
+    total_ratings: ["4", "4", "5"],
+    average_rating: 6,
+    created_dt: Date.now(),
+    favorites: [],
+    listings: [
+      {
+        listing_id: uuid(),
+        title: "Go Kart",
+        description: "The ride of your life",
+        status: "Active",
+        category: "Automobile",
+        image_path: "uploads/1f0be074-82a7-4950-82de-5f27ed7089ae-haircut.jpg",
+        date_created: Date.now(),
+      },
+      {
+        listing_id: uuid(),
+        title: "Electric Guitar",
+        description:
+          "Like an acoustic guitar, but better!",
+        status: "Active",
+        category: "Instrument",
+        image_path: "uploads/136065de-eb4b-4534-9323-79373a290595-mower.jpg",
+        date_created: Date.now(),
+      },
+    ],
+  },
+  {
+    name: "Keith",
+    email: "keithblack4290@gmail.com",
+    password: "password12345",
+    picture_path: "/tmp_uploads/f66a3bc8-0b42-49a2-9671-31045a707531-man 5.jpg",
+    total_ratings: ["1", "1", "1"],
+    average_rating: 1,
+    created_dt: Date.now(),
+    favorites: [],
+    listings: [
+      {
+        listing_id: uuid(),
+        title: "Jet Ski",
+        description: "1989 all original, like brand new",
+        status: "Active",
+        category: "Automobile",
+        image_path: "uploads/1f0be074-82a7-4950-82de-5f27ed7089ae-haircut.jpg",
+        date_created: Date.now(),
+      },
+      {
+        listing_id: uuid(),
+        title: "Babe Ruth Baseball Card",
+        description:
+          "Mint condition, never opened",
+        status: "Active",
+        category: "Collectables",
+        image_path: "uploads/136065de-eb4b-4534-9323-79373a290595-mower.jpg",
+        date_created: Date.now(),
+      },
+    ],
+  },
+  {
+    name: "Brice",
+    email: "brice.huisken@gmail.com",
+    password: "password12345",
+    picture_path: "/tmp_uploads/f66a3bc8-0b42-49a2-9671-31045a707531-man 5.jpg",
+    total_ratings: ["3", "3", "3"],
+    average_rating: 3,
+    created_dt: Date.now(),
+    favorites: [],
+    listings: [
+      {
+        listing_id: uuid(),
+        title: "Broncos Hat",
+        description: "Grey, was blue... signed by John Elway",
+        status: "Active",
+        category: "Clothing",
+        image_path: "uploads/1f0be074-82a7-4950-82de-5f27ed7089ae-haircut.jpg",
+        date_created: Date.now(),
+      },
+    ],
+  }
 ];
 
 db.User.remove({})
