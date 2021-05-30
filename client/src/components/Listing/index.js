9f818a22b602b05791ef775e8a9dd009bbd27b00
bahuisken <brice.huisken@gmail.com>
2021-05-30 16:03:11 -0600

    Catchup 5/30/21

--- client/src/components/Listing/index.js
+++ client/src/components/Listing/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
+import "./haggle.css";
 
 export function Listing() {
+  const [users, setUsers] = useState([]);
+  const [formObject, setFormObject] = useState({});
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  // Loads all books and sets them to books
+  function loadUsers() {
+    API.getListing()
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  }
+
   return (
-    <div>
-      <p>listing</p>
-    </div>
+    <>
+      {/* {users.map((user) => (
+        <div>
+          {user.listings.map((listing) => (
+            <p>{listing.title}</p>
+          ))}
+        </div>
+      ))} */}
+    </>
   );
 }
-
 export default Listing;
