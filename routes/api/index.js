33090e650a3713abac32e57f3285e5afaa096e08
bahuisken <brice.huisken@gmail.com>
2021-06-02 21:27:24 -0600

    profile stuff

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const listingRoutes = require("./listings");
+const profileRoutes = require("./profile");
 
-// Book routes
 router.use("/users", userRoutes);
 router.use("/listings", listingRoutes);
+router.use("/profile", profileRoutes);
 
 module.exports = router;
