32b83c64d9830b832f7a79dedd7c39ff8378228b
bahuisken <brice.huisken@gmail.com>
2021-06-08 19:56:03 -0600

    bcrypt in user model

--- client/src/pages/Listings.js
+++ client/src/pages/Listings.js
@@ -2,16 +2,12 @@ import React, { useEffect, useState } from "react";
 import API from "../utils/API";
 
 function Listings() {
-  // Setting our component's initial state
   const [listings, setListings] = useState([]);
-  // const [formObject, setFormObject] = useState({});
 
-  // Load all books and store them with setBooks
   useEffect(() => {
     loadListings();
   }, []);
 
-  // Loads all books and sets them to books
   function loadListings() {
     API.getListings()
       .then((res) => {
